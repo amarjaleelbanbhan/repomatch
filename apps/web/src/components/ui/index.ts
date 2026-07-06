0191fb6cc516432691772be9114c6c86de42fee7
amatjaleelbanbhan <banbhanamarjalil@gmail.com>
2026-07-07 00:11:19 +0500

    feat(ui): add barrel export index.ts to consolidate imports from components/ui

---
+++ apps/web/src/components/ui/index.ts
@@ -0,0 +1,32 @@
+/**
+ * RepoMatch UI Component Library
+ *
+ * Barrel export for all widgets.
+ * Import from "@/components/ui" in any page or component.
+ *
+ * Example:
+ *   import { Button, Badge, Card, Input } from "@/components/ui";
+ */
+
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Select } from "./Select";
+export { Checkbox } from "./Checkbox";
+export { Radio, RadioGroup } from "./Radio";
+export { Toggle } from "./Toggle";
+export { Badge, BadgeGroup } from "./Badge";
+export { Avatar } from "./Avatar";
+export { Card } from "./Card";
+export { Modal } from "./Modal";
+export { ToastProvider, useToast } from "./Toast";
+export type { ToastVariant } from "./Toast";
+export { Tooltip } from "./Tooltip";
+export { Tabs } from "./Tabs";
+export { Spinner } from "./Spinner";
+export { Skeleton, SkeletonGroup } from "./Skeleton";
+export { ProgressBar } from "./ProgressBar";
+export { Dropdown } from "./Dropdown";
+export type { DropdownItem } from "./Dropdown";
+export { Table } from "./Table";
+export type { TableColumn } from "./Table";
+export { Navbar } from "./Navbar";
